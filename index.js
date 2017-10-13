c903441cb11e993f4b5887c830b25c8fcae01331
Crista Cooper <aine3d@gmail.com>
2017-10-13 01:52:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
+}
 
-
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
 }
