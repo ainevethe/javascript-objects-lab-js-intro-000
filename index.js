a9b98395a7bdf4ef72ad53ff416066eb4c43e55d
Crista Cooper <aine3d@gmail.com>
2017-10-13 01:28:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var recipes = {};
+var recipes = {
+  flour: "2 cups",
+  eggs: "1 beaten",
+  sugar: "1 cup"
+};
 
 function updateObjectwithKeyandValue(object, key, value){
   Object.assign({}, {[key]:value});
